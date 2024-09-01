4ca11c26510800d235883612a432e79ec4622d23
Joshua Nestler <development@nekanu.com>
2024-09-01 19:44:06 +0200

    Remove Springboot und make Asp.Net more lightweight

--- rest-aspnet/Rest.AspNet/Data/BlogContext.cs
+++ rest-aspnet/Rest.AspNet/Data/BlogContext.cs
@@ -11,17 +11,24 @@ public class BlogContext(DbContextOptions<BlogContext> options) : DbContext(opti
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseMySQL(BuildConnectionStringFromEnvironment());
+        // optionsBuilder.UseMySQL(BuildConnectionStringFromEnvironment(), 
+        //    builder => builder.EnableRetryOnFailure());
+        optionsBuilder.UseInMemoryDatabase("blog").LogTo(Console.WriteLine, LogLevel.Warning);
     }
 
     private static string BuildConnectionStringFromEnvironment()
     {
-        string host = Environment.GetEnvironmentVariable("DATABASE_HOST") ?? throw new Exception("DATABASE_HOST environment variable not set");
-        string port = Environment.GetEnvironmentVariable("DATABASE_PORT") ?? throw new Exception("DATABASE_PORT environment variable not set");
-        string user = Environment.GetEnvironmentVariable("DATABASE_USER") ?? throw new Exception("DATABASE_USER environment variable not set");
-        string name = Environment.GetEnvironmentVariable("DATABASE_NAME") ?? throw new Exception("DATABASE_NAME environment variable not set");
-        string password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? throw new Exception("DATABASE_PASSWORD environment variable not set");
+        string host = Environment.GetEnvironmentVariable("DATABASE_HOST") 
+                      ?? throw new Exception("DATABASE_HOST environment variable not set");
+        string port = Environment.GetEnvironmentVariable("DATABASE_PORT") 
+                      ?? throw new Exception("DATABASE_PORT environment variable not set");
+        string user = Environment.GetEnvironmentVariable("DATABASE_USER") 
+                      ?? throw new Exception("DATABASE_USER environment variable not set");
+        string name = Environment.GetEnvironmentVariable("DATABASE_NAME") 
+                      ?? throw new Exception("DATABASE_NAME environment variable not set");
+        string password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") 
+                          ?? throw new Exception("DATABASE_PASSWORD environment variable not set");
 
-        return $"Server={host};Port={port};Database={name};User Id={user};Password={password}";
+        return $"Server={host};Port={port};Database={name};User Id={user};Password={password};";
     }
 }
--- rest-springboot/src/main/java/de/htwsaar/pimswks/restspringboot/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package de.htwsaar.pimswks.restspringboot.repositories;
-
-import de.htwsaar.pimswks.restspringboot.models.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "users", path = "users")
-public interface UserRepository extends CrudRepository<User, Long> {
-}
